42ee7ebc07d63b7e79d3eb24a53d69810a3b4fd0
Zakaria <zakaria@winu.fr>
2025-12-11 01:58:04 +0100

    add workflow for test

---
+++ src/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+#src/test.sh
+
+EXPECTED="Hello, Test!"
+
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
+
+
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+
+echo "test passed"
+
+
+exit 0
+
+else: 
+
+echo "test failed Expected '$EXPECTED' but got '$OUTPUT' "
+
+
+exit 1
+
+fi 
